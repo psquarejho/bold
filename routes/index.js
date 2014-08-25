62547afcb65968b6ff0085f0f830996d994829e0
Tor Livar Flugsrud <torlivar@torlivar.net>
2014-08-25 13:09:47 +0200

    Duplicate index page from old site

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
 
+var mainpages = [
+      {
+        path: '/',
+        name: 'home',
+        displayname: 'Home'
+      },
+      {
+        path: '/team',
+        name: 'team',
+        displayname: 'Team'
+      },
+      {
+        path: '/buyback',
+        name: 'buyback',
+        displayname: 'Buyback'
+      }
+    ];
+    
+var lookuppath = function(name) {
+  for (var i = 0; i < mainpages.length; i++) {
+    if (name === mainpages[i].name) return mainpages[i].displayname;
+  }
+  if (name === "info_newtobold")
+    return "/info/newtobold";
+}
+
+
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'BO-LD' });
+  res.render('index', { 
+    title: 'Home',
+    pages: mainpages,
+    current: 'home',
+    path: lookuppath
+     });
 });
 
 module.exports = router;
