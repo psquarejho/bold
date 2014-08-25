cad5bbae409a46d1aebbd81f6c1a965d75ead2db
Tor Livar Flugsrud <torlivar@torlivar.net>
2014-08-25 16:53:14 +0200

    Remove debug output

--- public/javascripts/buyback.js
+++ public/javascripts/buyback.js
@@ -7,7 +7,6 @@ $(function() {
   $("#buybackcalculator").submit(function(event) {
     event.preventDefault();
     var result = $("#raw_textarea").val();
-    alert(result);
     $.post('/buyback',
       {
         raw_paste: result
